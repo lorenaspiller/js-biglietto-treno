17f8004b8a9ba7d68b1d2c868a4520c2c877051a
Lorena Spiller <lorenaspiller@gmail.com>
2021-02-26 16:33:32 +0100

    creazione calcolo biglietto del treno

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*
+Calcolo del prezzo del biglietto del treno.
+Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65
+*/
+
+// 1. Richiesta numero di chilometri
+var chilometri = parseInt(prompt("Inserisci il numero di chilometri"));
+
+if ( isNaN(chilometri) ) {
+  var chilometri = parseInt(prompt("Inserisci il numero di chilometri. Deve essere un numero!"));
+}
+
+// 2. Richiedo l'eta'
+var eta = parseInt(prompt("Quanti anni hai?"));
+
+if ( isNaN(eta) ) {
+  var eta = parseInt(prompt("Quanti anni hai? La tua eta' deve essere un numero!"));
+}
+
+// 3. Calcolo prezzo del biglietto
+var prezzo = Math.floor(chilometri * 0.21);
+
+if ( eta < 18) {
+  alert("Il prezzo per il tuo biglietto e' " + ( prezzo - ( prezzo * 20 / 100 ) ) + " euro");
+} else if ( eta > 65) {
+  alert("Il prezzo per il tuo biglietto e' " + ( prezzo - ( prezzo * 40 / 100 ) ) + " euro");
+} else {
+  alert("Il prezzo per il tuo biglietto e' " + prezzo + " euro");
+}
